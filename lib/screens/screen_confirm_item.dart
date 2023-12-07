35717abb709f6debb848f1e48f7912f6f876ca0e
RobertoDev <roberto.info.bsi@gmail.com>
2023-12-07 00:54:40 -0300

    correção de ações botoes ok

--- lib/screens/screen_confirm_item.dart
+++ lib/screens/screen_confirm_item.dart
@@ -3,75 +3,88 @@ import 'package:flutter/services.dart';
 import 'dart:async';
 import 'package:lista_mercado/models/produto.dart';
 
-Future<double?> confirmItemScreen({BuildContext? context}) async{
+Future<double?> confirmItemScreen({BuildContext? context}) async {
   TextEditingController _textEditingController = TextEditingController();
   Completer<double?> completer = Completer();
+  bool isButtonEnabled = false;
 
   showModalBottomSheet(
     context: context!,
     builder: (BuildContext content) {
-      return Column(children: [
-        const Padding(
-          padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-          child: Text(
-            'Qual o valor do item?',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-          child: TextField(
-            inputFormatters: <TextInputFormatter>[
-              FilteringTextInputFormatter.digitsOnly
-            ],
-            autofocus: true,
-            keyboardType: TextInputType.number,
-            controller: _textEditingController,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 20),
-          ),
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          TextButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(
-                Colors.deepPurple[100], // Substitua pelo nome da cor desejada
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                child: Text(
+                  'Qual o valor do item?',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            onPressed: () {
-              completer
-                  .complete(null); // Completa o Future com null (cancelado)
-              Navigator.of(context).pop();
-            },
-            child: const Text('Cancelar'),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.disabled)) {
-                    return Colors.deepPurple[100]; // Cor quando desabilitado
-                  }
-                  return Colors.deepPurple; // Cor padrão
-                },
-              ),
-            ),
-            onPressed: _textEditingController.text.isEmpty
-                ? null
-                : () {
-                    double? price =
-                        double.tryParse(_textEditingController.text);
-                    completer.complete(price);
-                    _textEditingController.text = '';
-                    Navigator.of(context).pop();
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: TextField(
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  autofocus: true,
+                  keyboardType: TextInputType.number,
+                  controller: _textEditingController,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20),
+                  onChanged: (text) {
+                    setState(() {
+                      isButtonEnabled = text.isNotEmpty;
+                    });
                   },
-            child: const Text(
-              '     OK     ',
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ])
-      ]);
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.deepPurple[100],
+                      ),
+                    ),
+                    onPressed: () {
+                      completer.complete(null);
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancelar'),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: isButtonEnabled
+                          ? MaterialStateProperty.all(
+                              Colors.deepPurple,
+                            )
+                          : MaterialStateProperty.all(
+                              Colors.deepPurple[100],
+                            ),
+                    ),
+                    onPressed: isButtonEnabled
+                        ? () {
+                            double? price =
+                                double.tryParse(_textEditingController.text);
+                            completer.complete(price);
+                            _textEditingController.text = '';
+                            Navigator.of(context).pop();
+                          }
+                        : null,
+                    child: const Text(
+                      '  OK  ',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          );
+        },
+      );
     },
   );
 
--- lib/screens/screen_new_item.dart
+++ lib/screens/screen_new_item.dart
@@ -11,117 +11,136 @@ Future<Produto?> newItemScreen(BuildContext context) async {
   TextEditingController _textEditingControllerNewItemQuant =
       TextEditingController();
 
-  bool isButtonDisabled() {
-    return _textEditingControllerNewItem.text.isEmpty &&
-        _textEditingControllerNewItemQuant.text.isEmpty;
-  }
+  Completer<Produto?> completer = Completer();
 
   await showModalBottomSheet(
     isScrollControlled: true,
     context: context,
     builder: (BuildContext context) {
-      return SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-          ),
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-                child: Text(
-                  'Informe o novo item',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          bool isButtonDisabled() {
+            return _textEditingControllerNewItem.text.isEmpty ||
+                _textEditingControllerNewItemQuant.text.isEmpty;
+          }
+
+          _textEditingControllerNewItem.addListener(() {
+            setState(() {});
+          });
+
+          _textEditingControllerNewItemQuant.addListener(() {
+            setState(() {});
+          });
+
+          return SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: TextField(
-                  autofocus: true,
-                  keyboardType: TextInputType.text,
-                  controller: _textEditingControllerNewItem,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 20),
-                  decoration: const InputDecoration(
-                    labelText: 'Produto',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: TextField(
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  keyboardType: TextInputType.number,
-                  controller: _textEditingControllerNewItemQuant,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 20),
-                  decoration: const InputDecoration(
-                    labelText: 'Quantidade',
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: Column(
                 children: [
-                  TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Colors.deepPurple[
-                            100], // Substitua pelo nome da cor desejada
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                    child: Text(
+                      'Informe o novo item',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: TextField(
+                      autofocus: true,
+                      keyboardType: TextInputType.text,
+                      controller: _textEditingControllerNewItem,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        labelText: 'Produto',
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Cancelar'),
                   ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.resolveWith<Color?>(
-                        (Set<MaterialState> states) {
-                          if (states.contains(MaterialState.disabled)) {
-                            return Colors
-                                .deepPurple[100]; // Cor quando desabilitado
-                          }
-                          return Colors.deepPurple; // Cor padrão
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                      keyboardType: TextInputType.number,
+                      controller: _textEditingControllerNewItemQuant,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        labelText: 'Quantidade',
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.deepPurple[100],
+                          ),
+                        ),
+                        onPressed: () {
+                          completer.complete(null);
+                          Navigator.of(context).pop();
                         },
+                        child: const Text('Cancelar'),
                       ),
-                    ),
-                    onPressed: isButtonDisabled()
-                        ? null
-                        : () {
-                            newItem = Produto.newItemList(
-                              descricao:
-                                  _textEditingControllerNewItem.text.toString(),
-                              quantidade: int.parse(
-                                  _textEditingControllerNewItemQuant.text),
-                            );
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.disabled)) {
+                                return Colors
+                                    .deepPurple[100]; // Cor quando desabilitado
+                              }
+                              return Colors.deepPurple; // Cor padrão
+                            },
+                          ),
+                        ),
+                        onPressed: isButtonDisabled()
+                            ? null
+                            : () {
+                                setState(() {
+                                  newItem = Produto.newItemList(
+                                    descricao:
+                                        _textEditingControllerNewItem.text,
+                                    quantidade: int.parse(
+                                        _textEditingControllerNewItemQuant
+                                            .text),
+                                  );
 
-                            _textEditingControllerNewItem.text = '';
-                            _textEditingControllerNewItemQuant.text = '';
+                                  _textEditingControllerNewItem.text = '';
+                                  _textEditingControllerNewItemQuant.text = '';
+                                });
 
-                            Navigator.of(context).pop();
-                          },
-                    child: const Text(
-                      '     OK     ',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+                                completer.complete(newItem);
+                                Navigator.of(context).pop();
+                              },
+                        child: const Text(
+                          '  OK  ',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        ),
+              ),
+            ),
+          );
+        },
       );
     },
   );
 
+  // Adicione informações padrão ao newItem se necessário
   newItem?.barras = 12345678.toString();
   newItem?.precoAtual = 5.0;
   newItem?.historicoPreco = [4, 5, 5, 5];
 
-  return newItem;
+  return completer.future;
 }
